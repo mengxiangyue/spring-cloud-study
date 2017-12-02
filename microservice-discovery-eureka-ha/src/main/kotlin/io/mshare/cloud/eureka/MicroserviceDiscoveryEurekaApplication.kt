c87716a8a237fd9b43c24ad9befcff03b3b284a1
Xiangyue Meng <598660766@qq.com>
2017-12-02 12:36:09 +0800

    eureka 高可用的配置的时候 如果有校验 需要配置用户名密码 使用random产生端口 有可能注册到eureka失败

--- microservice-discovery-eureka-ha/src/main/kotlin/io/mshare/cloud/eureka/MicroserviceDiscoveryEurekaApplication.kt
+++ microservice-discovery-eureka-ha/src/main/kotlin/io/mshare/cloud/eureka/MicroserviceDiscoveryEurekaApplication.kt
@@ -2,10 +2,12 @@ package io.mshare.cloud.eureka
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableDiscoveryClient
 class MicroserviceDiscoveryEurekaApplication
 
 fun main(args: Array<String>) {
